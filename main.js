5f3ce1bc30a7010824aa452a6cf8c7d8e1d800d7
Merv1903 <merve1995samur@gmail.com>
2026-01-23 16:13:09 +0100

    Aktualisiere Dateien

--- main.js
+++ main.js
@@ -1,82 +1,71 @@
-let menuObjects = {
-  name: "Akakiko",
-  restaurant_cover_img: "./img/akahito_cover.jpg",
-  logo: "./img/akahito_logo.png",
-  rating: "⭐ 4,7/5(250+)",
+let allPokemons = [];
+let currentOffset = 0;
+let limit = 20;
+let isLoading = false;
 
-  categories: {
-    platten: "Platten",
-    suppen: "Suppen",
-    salate: "Salate",
-  },
+async function init() {
+  await loadNextPokemons();
+}
 
-  categoryImages: {
-    platten: "./img/sushi_cover.jpg",
-    suppen: "./img/soup_cover.jpg",
-    salate: "./img/salad_cover.jpg",
-  },
-  menu: [
-    {
-      name: "Rapongi Platte",
-      price: 18.4,
-      description:
-        "10 California Sake Maki, 10 California Maki, 10 Tempura Maki",
-      category: "platten",
-      dish_img: "./img/rapongi_plate.jpg",
-      likes: 27,
-      liked: true,
-    },
-    {
-      name: "Shibuya Platte",
-      price: 66.0,
-      description: "21 Lachs Shushi, 14 Maki (Lachs + Avocado)",
-      category: "platten",
-      dish_img: "./img/shibuya_plate.jpg",
-      likes: 40,
-      liked: true,
-    },
+function showLoader() {
+  document.getElementById("loader").classList.remove("d-none");
+}
 
-    {
-      name: "Miso Shiru, 250ml",
-      price: 3.7,
-      description: "Suppe aus Sojabohnenpaste",
-      category: "suppen",
-      dish_img: "./img/miso_soup.jpg",
-      likes: 39,
-      liked: true,
-    },
-    {
-      name: "Sweet Sour Soup, 340ml (pikant)",
-      price: 4.7,
-      description: "mit Hühnerbrust, Morcheln, roter Paprika und Eistreifen",
-      category: "suppen",
-      dish_img: "./img/sweetsour_soup.jpg",
-      likes: 22,
-      liked: true,
-    },
- 
-    {
-      name: "Wakame Salad (pikant)",
-      price: 5.8,
-      description:
-        "Seetangsalat mit Lemon-Dressing, kleinen Chilischoten und Blütenpfeffer",
-      category: "salate",
-      dish_img: "./img/wakame_salad.jpg",
-      likes: 14,
-      liked: true,
-    },
-    {
-      name: "Hit! Kimchi (scharf)",
-      price: 5.1,
-      description:
-        "scharfer fermentierter Chinakohl mit Ingwer und reichtlich Knoblauch in scharfem Paprikapulver. Mit probiotischen Kulturen.",
-      category: "salate",
-      dish_img: "./img/kimchi_salad.jpg",
-      likes: 18,
-      liked: true,
-    },
-  ],
-  imprint_icon: "./img/building.png",
-  imprint: "Impressum",
-  address: "Akakiko<br>Am Platz 3<br>1130 Wien",
-};
+function hideLoader() {
+  document.getElementById("loader").classList.add("d-none");
+}
+
+async function loadNextPokemons() {
+  if (isLoading) {
+    return;
+  }
+
+  isLoading = true;
+  showLoader();
+
+  let data = await loadPokemons(currentOffset, limit);
+
+  for (let i = 0; i < data.results.length; i++) {
+    let pokemonData = await loadSinglePokemon(data.results[i].url);
+    allPokemons.push(pokemonData);
+    renderPokemon(pokemonData, allPokemons.length - 1);
+  }
+
+  currentOffset = currentOffset + limit;
+  hideLoader();
+  isLoading = false;
+}
+
+
+function renderPokemon(pokemon, index) {
+  let container = document.getElementById("pokemons_container");
+
+  container.innerHTML += `
+    <div class="pokemon-card" onclick="openOverlay(${index})">
+      <h3>#${pokemon.id} ${pokemon.name}</h3>
+      <img src="${pokemon.sprites.front_default}">
+    </div>
+  `;
+}
+
+
+function openOverlay(index) {
+  let pokemon = allPokemons[index];
+
+  document.getElementById("overlay").classList.remove("d-none");
+
+  document.getElementById("overlay_content").innerHTML = `
+    <h2>${pokemon.name}</h2>
+    <img src="${pokemon.sprites.front_default}">
+    <p>Height: ${pokemon.height}</p>
+    <p>Weight: ${pokemon.weight}</p>
+    <p>Attack: ${pokemon.stats[1].base_stat}</p>
+    <p>Defense: ${pokemon.stats[2].base_stat}</p>
+  `;
+}
+
+
+function closeOverlay() {
+  document.getElementById("overlay").classList.add("d-none");
+}
+
